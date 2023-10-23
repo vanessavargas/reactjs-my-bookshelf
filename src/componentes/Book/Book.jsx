2410eda63e9b4cb86988d49f9acf8ddc9244f878
Vanessa Vargas <vanessa.vargas4f@gmail.com>
2023-10-23 18:28:38 -0300

    Corrigindo método delete

--- src/componentes/Book/Book.jsx
+++ src/componentes/Book/Book.jsx
@@ -4,11 +4,11 @@ import { AiFillCloseCircle } from "react-icons/ai";
 const Book = ({ book, corDeFundo, aoDeletar }) => {
   return (
     <div className="book">
-      <AiFillCloseCircle size={25} className="livro__deletar" onClick={aoDeletar} />
-      <div className="book__cabecalho" style={{ backgroundColor: corDeFundo }}>
+      <AiFillCloseCircle size={25} className="book__delete" onClick={() => aoDeletar(book)}/>
+      <div className="book__header" style={{ backgroundColor: corDeFundo }}>
         <img src={book.image} alt={book.title} />
       </div>
-      <div className="book__rodape">
+      <div className="book__footer">
         <h4>{book.title}</h4>
         <h5>{book.author}</h5>
       </div>
